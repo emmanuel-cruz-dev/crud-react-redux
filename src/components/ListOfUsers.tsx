bda3b38846c156b24ea66ce2b2e1b5ba2e8efa7c
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-02 11:22:23 -0300

    useUserActions hook creado e implementado en ListOfUsers para separar lógica

--- src/components/ListOfUsers.tsx
+++ src/components/ListOfUsers.tsx
@@ -9,20 +9,12 @@ import {
   TableRow,
   Title,
 } from "@tremor/react";
-import { useAppDispatch, useAppSelector } from "../hooks/store";
-import { deleteUserById, UserId } from "../store/users/slice";
-
-// import { useUserActions } from "../hooks/useUserActions";
+import { useAppSelector } from "../hooks/store";
+import { useUserActions } from "../hooks/useUserActions";
 
 export function ListOfUsers() {
   const users = useAppSelector((state) => state.users);
-  const dispatch = useAppDispatch();
-
-  // const { removeUser } = useUserActions();
-
-  const handleRemoveUser = (id: UserId) => {
-    dispatch(deleteUserById(id));
-  };
+  const { removeUser } = useUserActions();
 
   return (
     <Card>
@@ -75,7 +67,7 @@ export function ListOfUsers() {
                     />
                   </svg>
                 </button>
-                <button onClick={() => handleRemoveUser(item.id)} type="button">
+                <button onClick={() => removeUser(item.id)} type="button">
                   <svg
                     aria-label="Remove element"
                     xmlns="http://www.w3.org/2000/svg"

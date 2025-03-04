fc1b17026192fc41fee1cac9a414f95579ca4d91
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-04 07:51:00 -0300

    cambios en initialState dentro de slice.ts

--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -1,18 +1,6 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
-export type UserId = string;
-
-export interface User {
-  name: string;
-  email: string;
-  github: string;
-}
-
-export interface UserWithId extends User {
-  id: UserId;
-}
-
-const initialState: UserWithId[] = [
+const DEFAULT_STATE = [
   {
     id: "1",
     name: "Emmanuel Cruz",
@@ -32,6 +20,25 @@ const initialState: UserWithId[] = [
     github: "midudev",
   },
 ];
+
+export type UserId = string;
+
+export interface User {
+  name: string;
+  email: string;
+  github: string;
+}
+
+export interface UserWithId extends User {
+  id: UserId;
+}
+
+const initialState: UserWithId[] = () => {
+  const persistedState = localStorage.getItem("__redux__state__");
+  if (persistedState) {
+    return JSON.parse(persistedState).users;
+  }
+};
 
 export const usersSlice = createSlice({
   name: "users",

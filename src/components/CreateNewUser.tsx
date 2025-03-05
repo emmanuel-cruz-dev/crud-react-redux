134803debd847856f4d694b952186cc2d33c8b85
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-05 07:36:07 -0300

    CreateNewUser funcional implementada con addUser

--- src/components/CreateNewUser.tsx
+++ src/components/CreateNewUser.tsx
@@ -1,13 +1,28 @@
 import { Button, Card, TextInput, Title } from "@tremor/react";
+import { useUserActions } from "../hooks/useUserActions";
+import React from "react";
 
 export function CreateNewUser() {
+  const { addUser } = useUserActions();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormEvent>) => {
+    const form = event.target;
+    const formData = new FormData(form);
+
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const github = formData.get("github") as string;
+
+    addUser({ name, email, github });
+  };
+
   return (
     <Card style={{ marginTop: "16px" }}>
       <Title>Create New User</Title>
-      <form>
-        <TextInput placeholder="Aquí el nombre" />
-        <TextInput placeholder="Aquí el mail" />
-        <TextInput placeholder="Aquí el usuario de GitHub" />
+      <form onSubmit={handleSubmit}>
+        <TextInput name="name" placeholder="Aquí el nombre" />
+        <TextInput name="email" placeholder="Aquí el email" />
+        <TextInput name="github" placeholder="Aquí el usuario de GitHub" />
 
         <div>
           <Button type="submit" style={{ marginTop: "16px" }}>
